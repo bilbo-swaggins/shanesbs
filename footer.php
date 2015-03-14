fafa7f42b7e73c74a09addd8db57ec5dc717d961
Eric Willis <ewillis182@gmail.com>
2015-03-13 21:44:42 -0700

    Added mailing list to footer

--- footer.php
+++ footer.php
@@ -12,55 +12,47 @@
 						<div class="col-sm-4">
 							<h3>Mailing List</h3>
 							<!-- Begin MailChimp Signup Form -->
-							<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
-							<style type="text/css">
-							#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-							/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-							We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-							</style>
-							<div id="mc_embed_signup">
+							<div id="mc_embed_signup" class="mail-list">
 								<form action="//shanescards.us9.list-manage.com/subscribe/post?u=909310b6a5e186a43789e18ef&amp;id=f24e3a21cf" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-							<div id="mc_embed_signup_scroll">
+								<div id="mc_embed_signup_scroll">
+								<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+								<div class="mc-field-group form-group">
+									<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
+								</label>
+									<input type="text" value="" name="FNAME" class="required form-control" id="mce-FNAME">
+								</div>
+								<div class="mc-field-group form-group">
+									<label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
+								</label>
+									<input type="text" value="" name="LNAME" class="required form-control" id="mce-LNAME">
+								</div>
+								<div class="mc-field-group form-group">
+									<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+								</label>
+									<input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL">
+								</div>
+								<div class="mc-field-group input-group checkbox">
+								    <strong>What newsletter would you like to receive? </strong>
+								    <ul><li><input type="checkbox" value="1" name="group[14881][1]" id="mce-group[14881]-14881-0"><label for="mce-group[14881]-14881-0">Magic: The Gathering</label></li>
+								<li><input type="checkbox" value="2" name="group[14881][2]" id="mce-group[14881]-14881-1"><label for="mce-group[14881]-14881-1">Yu-gi-oh</label></li>
+								<li><input type="checkbox" value="4" name="group[14881][4]" id="mce-group[14881]-14881-2"><label for="mce-group[14881]-14881-2">Pokemon TCG</label></li>
+								<li><input type="checkbox" value="8" name="group[14881][8]" id="mce-group[14881]-14881-3"><label for="mce-group[14881]-14881-3">Card Fight Vanguard</label></li>
+								<li><input type="checkbox" value="16" name="group[14881][16]" id="mce-group[14881]-14881-4"><label for="mce-group[14881]-14881-4">Sports</label></li>
+								<li><input type="checkbox" value="32" name="group[14881][32]" id="mce-group[14881]-14881-5"><label for="mce-group[14881]-14881-5">Board Games</label></li>
+								</ul>
+								</div>
+									<div id="mce-responses" class="clear">
+										<div class="response" id="mce-error-response" style="display:none"></div>
+										<div class="response" id="mce-success-response" style="display:none"></div>
+									</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+								    <div style="position: absolute; left: -5000px;"><input type="text" name="b_909310b6a5e186a43789e18ef_f24e3a21cf" tabindex="-1" value=""></div>
+								    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn btn-default"></div>
+								    </div>
+								</form>
+							</div>
 
-							<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
-							<div class="mc-field-group">
-							<label for="mce-FNAME">First Name  <span class="asterisk">*</span>
-							</label>
-							<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
-							</div>
-							<div class="mc-field-group">
-							<label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
-							</label>
-							<input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
-							</div>
-							<div class="mc-field-group">
-							<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-							</label>
-							<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-							</div>
-							<div class="mc-field-group input-group">
-							<strong>What newsletter would you like to receive? </strong>
-							<ul><li><input type="checkbox" value="1" name="group[14881][1]" id="mce-group[14881]-14881-0"><label for="mce-group[14881]-14881-0">Magic: The Gathering</label></li>
-							<li><input type="checkbox" value="2" name="group[14881][2]" id="mce-group[14881]-14881-1"><label for="mce-group[14881]-14881-1">Yu-gi-oh</label></li>
-							<li><input type="checkbox" value="4" name="group[14881][4]" id="mce-group[14881]-14881-2"><label for="mce-group[14881]-14881-2">Pokemon TCG</label></li>
-							<li><input type="checkbox" value="8" name="group[14881][8]" id="mce-group[14881]-14881-3"><label for="mce-group[14881]-14881-3">Card Fight Vanguard</label></li>
-							<li><input type="checkbox" value="16" name="group[14881][16]" id="mce-group[14881]-14881-4"><label for="mce-group[14881]-14881-4">Sports</label></li>
-							<li><input type="checkbox" value="32" name="group[14881][32]" id="mce-group[14881]-14881-5"><label for="mce-group[14881]-14881-5">Board Games</label></li>
-							</ul>
-							</div>
-							<div id="mce-responses" class="clear">
-							<div class="response" id="mce-error-response" style="display:none"></div>
-							<div class="response" id="mce-success-response" style="display:none"></div>
-							</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-							<div style="position: absolute; left: -5000px;"><input type="text" name="b_909310b6a5e186a43789e18ef_f24e3a21cf" tabindex="-1" value=""></div>
-							<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-							</div>
-							</form>
-							</div>
-							<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 							<!--End mc_embed_signup-->
-						</div>
-						<!-- End Mailing List  -->
+							</div>
 						<div class="col-sm-4">
 							<nav class="clearfix footer-links">
 								<?php wp_bootstrap_footer_links(); // Adjust using Menus in Wordpress Admin ?>
